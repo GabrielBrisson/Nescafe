cdc1c679088a4148ef72538fb66d26ca64809e2f
Gabriel Brisson <gabrielbrisson02@gmail.com>
2022-03-26 19:34:18 -0300

    Splash and logo asset

---
+++ app/src/main/java/com/dolcegusto/nescafe/features/main/ui/activity/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dolcegusto.nescafe.features.main.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dolcegusto.nescafe.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
--- app/src/main/java/com/dolcegusto/nescafe/features/splash/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/dolcegusto/nescafe/features/splash/ui/activity/SplashActivity.kt
@@ -1,12 +1,21 @@
 package com.dolcegusto.nescafe.features.splash.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 import com.dolcegusto.nescafe.R
+import com.dolcegusto.nescafe.databinding.ActivitySplashBinding
+
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
