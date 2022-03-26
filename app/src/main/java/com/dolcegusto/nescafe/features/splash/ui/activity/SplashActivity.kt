84fc6b7ef712a89438a47909602d7e96c87ad574
Gabriel Brisson <gabrielbrisson02@gmail.com>
2022-03-26 20:11:39 -0300

    Transaction logic to MainActivity.kt

--- app/src/main/java/com/dolcegusto/nescafe/features/splash/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/dolcegusto/nescafe/features/splash/ui/activity/SplashActivity.kt
@@ -1,8 +1,12 @@
 package com.dolcegusto.nescafe.features.splash.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.dolcegusto.nescafe.databinding.ActivitySplashBinding
+import com.dolcegusto.nescafe.features.main.ui.activity.MainActivity
 
 
 class SplashActivity : AppCompatActivity() {
@@ -14,5 +18,9 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }, 2500)
     }
 }
